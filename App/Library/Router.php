cc5c72d2366e2c011656a2d9f56cc7cd3e93d7d2
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-10-09 12:12:52 -0300

    Aula #01: Criadas as Classes responsáveis pelo Roteamento e gerenciamento das Rotas do site

---
+++ App/Library/Router.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Library;
+
+use App\Library\Route;
+use App\Library\Controller;
+
+class Router {
+
+    private array $routes =[];
+
+    public function add(string $uri, string $request, string $controller)
+    {
+        $this->routes[] = new Route($uri, $request, $controller);
+    }
+
+    public function init()
+    {
+        foreach ($this->routes as $route) {
+            if($route->match($route)) {
+                return (new Controller)->call($route);
+            }
+        }
+
+        return (new Controller)->call(new Route('/404', 'GET', 'NotFoundController:index'));
+    }
+
+}
--- public/index.php
+++ public/index.php
@@ -1,13 +1,26 @@
 <?php
 
-use app\library\Cart;
-use app\library\Product;
+use App\Library\Router;
 
 require '../vendor/autoload.php';
 
-session_start();
+try {
+    $route = new Router;
+    $route->add('/', 'GET', 'HomeController:index');
+    $route->add('/cart', 'GET', 'CartController:index');
+    $route->add('/cart/add', 'GET', 'CartController:store');
+    $route->add('/contact', 'GET', 'ContactController:inde');
+    $route->init();    
+} catch (Exception $e) {
+    // dump('Error: '.$e->getMessage().' - ['.$e->getFile().'] ('.$e->getLine().')');
+    echo "<pre>";
+    print_r("  Error: {$e->getMessage()} 
+    - Arquivo: {$e->getFile()} 
+    -- Linha: {$e->getLine()}");
+    echo "</pre>";
+}
 
-$products = [
+/* $products = [
     1 => ['id' => 1, 'name' => 'geladeira', 'price' => 1000, 'quantity' => 1],
     2 => ['id' => 2, 'name' => 'mouse', 'price' => 100, 'quantity' => 1],
     3 => ['id' => 3, 'name' => 'teclado', 'price' => 10, 'quantity' => 1],
@@ -31,34 +44,4 @@ if(isset($_GET['id'])) {
 
 }
 
-dump($_SESSION['cart']['products'] ?? ['Sem produtos']);
-
-?>
-
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Sem título</title>
-</head>
-<body>
-    <a href="/mycart.php">Go to Cart</a>
-    <ul>
-        <?php foreach($products as $product): ?>
-            <li>
-                <?= ucfirst($product['name']) ?> | 
-                <a href="?id=<?= $product['id'] ?>">Add</a> | 
-                R$ <?= number_format($product['price'],2 , ',', '.') ?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-    <!-- <ul>
-        <li>Geladeira <a href="?id=1">Add</a> R$ 1000</li>
-        <li>Mouse <a href="?id=2">Add</a> R$ 100</li>
-        <li>Teclado <a href="?id=3">Add</a> R$ 10</li>
-        <li>Monitor <a href="?id=4">Add</a> R$ 5000</li>
-    </ul> -->
-</body>
-</html>
+dump($_SESSION['cart']['products'] ?? ['Sem produtos']); */
