63921294f30d6968ee650c3553f4052ef3b894f3
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-10-09 20:52:54 -0300

    Aula #04: Adicionado Meta TAGS

--- App/Views/master.php
+++ App/Views/master.php
@@ -2,14 +2,25 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8" />
+
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+
+    <meta name="author" content="Rafel Santoni" />
+    <meta name="keywords" content="Loja Vitural, Mini Loja, HTML, CSS, JavaScript, PHP, OO">
+    <meta name="description" content="Mini Loja Virtual desenvolvida durante estudos sobre PHPOO" />
+
+    <meta property="og:title" content="Mini Loja Virtual" />
+    <meta property="og:description" content="Mini Loja Virtual desenvolvida durante estudos sobre PHPOO" />
+    <meta property="og:url" content="https://rafasantoni.heliohost.us/mini-loja/" />
+    <meta property="og:image" content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png" />
+    <meta property="og:type" content="website" />
+    <meta property="og:locale" content="pt_BR" />
 
     <title>Mini Loja - RS-Dev</title>
 
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico" />
+    <!-- <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico" /> -->
+    <link rel="icon" type="image/x-icon" href="https://img.icons8.com/color/120/shop.png" />
     
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
